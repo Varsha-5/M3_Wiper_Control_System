6b9c810913b1db4ee43203ee0c299573d8b25e3a
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-05-15 13:08:26 +0530

    Create Wiper_Control_System.h

---
+++ 3_Implementation/inc/Wiper_Control_System.h
@@ -0,0 +1,19 @@
+#ifndef WIPER_CONROL_SYSTEM_H_INCLUDED
+
+#define WIPER_CONTROL_SYSTEM_H_INCLUDED
+
+void WIPER_SYSTEM_OFF(void);
+
+void WIPER_SYSTEM_ON(void);
+
+void LOCK_CAR(void);
+
+void UNLOCK_CAR(void);
+
+void DELAY(void);
+
+void CLOCKWISE_MOVEMENT(void);
+
+void ANTICLOCKWISE_MOVEMENT(void);
+
+#endif 
