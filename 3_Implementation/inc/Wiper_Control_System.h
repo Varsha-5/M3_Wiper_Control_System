56e9af8e500c81c5a52a0cfb995d3ee96a9b6cfa
Varsha-5 <101313943+Varsha-5@users.noreply.github.com>
2022-05-15 21:10:43 +0530

    Update Wiper_Control_System.h

--- 3_Implementation/inc/Wiper_Control_System.h
+++ 3_Implementation/inc/Wiper_Control_System.h
@@ -1,19 +1,7 @@
-#ifndef WIPER_CONROL_SYSTEM_H_INCLUDED
+#ifndef WIPER_SYSTEM_H_INCLUDED
 
-#define WIPER_CONTROL_SYSTEM_H_INCLUDED
+#define WIPER_SYSTEM_H_INCLUDED
 
-void WIPER_SYSTEM_OFF(void);
-
-void WIPER_SYSTEM_ON(void);
-
-void LOCK_CAR(void);
-
-void UNLOCK_CAR(void);
-
-void DELAY(void);
-
-void WIPER_MOVEMENT(void);
-
-
-
+void Blink_LED(void);
+void Making_a_delay(volatile int time);
 #endif 
